30168494ccf159230d738c587aece4aaf01134f5
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-06-21 09:35:05 +0700

    Create 2455. Average Value of Even Numbers That Are Divisible by Three.java

---
+++ 2455. Average Value of Even Numbers That Are Divisible by Three.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0 && nums[i]%3==0){
+                sum+=nums[i];
+                count++;
+            }
+        }
+        if(sum>0)
+        return (sum/count);
+        else
+        return 0;
+    }
+}
