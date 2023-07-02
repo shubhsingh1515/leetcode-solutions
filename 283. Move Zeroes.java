3f4a6a734eb810e296ec47f6b4c1a8aa4a48b5cf
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-07-02 21:43:32 +0700

    Create 283. Move Zeroes.java

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int k=nums[i];
+                nums[i]=nums[count];
+                nums[count]=k;
+                count++;
+            }
+        }
+    }
+}
