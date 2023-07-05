db7b88b0ac0a9cc4462f3c36a4550069d53bfffa
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-07-05 21:22:42 +0700

    Create 1323. Maximum 69 Number.java

---
+++ 1323. Maximum 69 Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximum69Number (int num) {
+        StringBuilder s=new StringBuilder();
+        s.append(num);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='6'){
+                s.setCharAt(i,'9');
+                break;
+            }
+        }
+        return Integer.parseInt(s.toString());
+    }
+}
