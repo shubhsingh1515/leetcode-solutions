01bd215b8de6c8376b3508aefadd8694ac3215f9
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-07-14 00:51:26 +0700

    Create 575. Distribute Candies.java

---
+++ 575. Distribute Candies.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int num : candyType){
+            set.add(num);
+        }
+        if(set.size()>=candyType.length/2)
+        return candyType.length/2;
+        return set.size();
+    }
+}
