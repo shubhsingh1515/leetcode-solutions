fce2c9cfbb6f69ed1084dd310001445090a0a50f
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-07-28 00:53:00 +0700

    Create 1991. Find the Middle Index in Array.java

---
+++ 1991. Find the Middle Index in Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int lsum=0;
+        for(int i=0;i<n;i++){
+            if(lsum==sum-nums[i]){
+                return i;
+            }
+            lsum+=nums[i];
+            sum-=nums[i];
+        }
+        return -1;
+    }
+}
