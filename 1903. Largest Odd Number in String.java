135fd7b6975240ec4a17fb48013a8bf1032c3dfd
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-09-30 03:05:06 +0700

    Create 1903. Largest Odd Number in String.java

---
+++ 1903. Largest Odd Number in String.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String largestOddNumber(String num) {
+        for(int i=num.length()-1;i>=0;i--){
+            int ch=num.charAt(i);
+            if(ch%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
