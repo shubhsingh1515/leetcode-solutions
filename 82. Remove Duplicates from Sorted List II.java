5332564d6942151da0e82a02b7dd7db62f3ec5b6
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2023-10-13 00:01:33 +0700

    Create 82. Remove Duplicates from Sorted List II.java

---
+++ 82. Remove Duplicates from Sorted List II.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummy  =new ListNode(0);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy.next;
+
+        while(fast!=null){  
+             
+            while(fast.next!=null && fast.val==fast.next.val){
+
+                fast=fast.next;
+            }    
+            if(slow.next!=fast){
+                slow.next=fast.next;
+                fast=slow.next;
+            }
+            else{
+            slow=slow.next;
+            fast=fast.next;
+            }      
+        }
+        return dummy.next;
+    }   
+}
