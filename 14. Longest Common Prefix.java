9b96cc7aabb5c4146495b394b73640a2da499f98
Shubham Singh <120304300+shubhsingh1515@users.noreply.github.com>
2025-10-12 08:36:46 +0530

    Implement longest common prefix algorithm

---
+++ 14. Longest Common Prefix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length<1) {
+			return "";
+		}
+        else if(strs.length == 1){
+            return strs[0];
+        }
+		else {
+			Arrays.sort(strs);
+			String first = strs[0];
+			String last = strs[strs.length-1];
+			
+			int i=0;
+			while(i < first.length() && i<last.length() && first.charAt(i)==last.charAt(i)) {
+				i++;
+			}
+			return first.substring(0,i);
+		}
+    }
+}
